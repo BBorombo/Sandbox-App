d2df139e742e61b8fb353a2af0297c6025851c90
Borombo <e.rombo@yahoo.com>
2017-04-18 21:16:18 +0200

    Gradle change + Init buttons MainActivity

--- app/src/main/java/com/borombo/sandboxapp/MainActivity.java
+++ app/src/main/java/com/borombo/sandboxapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.borombo.sandboxapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button mailButton = (Button) findViewById(R.id.buttonMail);
+        Button gMailButton = (Button) findViewById(R.id.buttonGMail);
+        Button facebookButton = (Button) findViewById(R.id.buttonFb);
+
+        mailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MailAuthActivity.class);
+            }
+        });
+
+        gMailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        facebookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
