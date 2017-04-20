96a3bf738daa1b4872f8edbb3b3809ef04892deb
BBorombo <e.rombo@yahoo.com>
2017-04-20 19:50:43 +0200

    Main Design

--- app/src/main/java/com/borombo/sandboxapp/firebase/FirebaseMainActivity.java
+++ app/src/main/java/com/borombo/sandboxapp/firebase/FirebaseMainActivity.java
@@ -1,7 +1,13 @@
 package com.borombo.sandboxapp.firebase;
 
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.borombo.sandboxapp.R;
 
@@ -11,5 +17,20 @@ public class FirebaseMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_firebase_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Firebase");
+        actionBar.setBackgroundDrawable(new ColorDrawable(ContextCompat.getColor(this,R.color.firebase_primary)));
+
+
+        ConstraintLayout authView = (ConstraintLayout) findViewById(R.id.firebaseAuthView);
+        authView.setClickable(true);
+        authView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FirebaseMainActivity.this, AuthMainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
